03805bebfaa18cfd367b6bd286df12465d11c2fc
kshitijstc <kshitijstc@gmail.com>
2024-06-29 17:06:56 +0530

    RB101

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ bgColor, color }) {
   return (
     <nav className='navbar w-full flex px-10 py-10 justify-between items-center' style={{ backgroundColor: bgColor, color: color }}>
       <img src={roboClubImage} alt="" className="h-[50px] w-[57px] ml-1 mr-5 mb-1"/>
-      <h1 className=' h-[32px] ' alt="logo"><Link to='/'>ROBOTICS<span className='ml-1'>102</span></Link></h1>
+      <h1 className=' h-[32px] ' alt="logo"><Link to='/'>ROBOTICS<span className='ml-1'>101</span></Link></h1>
       
       <ul className="list-none flex flex-1 px-[15%] justify-around  items-center">
         <li>
