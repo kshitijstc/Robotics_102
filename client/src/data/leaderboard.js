92971289b298fdc3eeabba5268d42cd26856b8d1
Preetam G <118665778+preetam-g@users.noreply.github.com>
2024-12-26 14:40:55 +0530

    updated learderboard logic

--- client/src/data/leaderboard.js
+++ client/src/data/leaderboard.js
@@ -1,29 +1,36 @@
 const ldata =[
-  // {
-  //   name:"Preetam",
-  //   course:"Fusion 360",
-  //   total:91.69,
-  // },
-  // {
-  //   name:"Krishna",
-  //   course:"ROS",
-  //   total:89,
-  // },
-  // {
-  //   name:"Preetam",
-  //   course:"OpenCV",
-  //   total:91.69,
-  // },
-  // {
-  //   name:"Krishna",
-  //   course:"IOT",
-  //   total:89,
-  // },
-  // {
-  //   name:"Kshitij",
-  //   course:"IOT",
-  //   total:70,
-  // },
+  {
+    name:"Preetam",
+    course:"Fusion",
+    total:91.69,
+  },
+  {
+    name:"Krishna",
+    course:"ROS",
+    total:9,
+  },
+  {
+    name:"Krishna",
+    course:"ROS",
+    total:89,
+  },
+  {
+    name:"Preetam",
+    course:"OpenCV",
+    total:91.69,
+  },
+  {
+    name:"Krishna",
+    course:"IOT",
+    total:89,
+  },
+  {
+    name:"Kshitij",
+    course:"IOT",
+    total:70,
+  },
 ];
-const sortedData = ldata.sort((a,b)=> b.total-a.total);
+
+
+const sortedData = ldata.sort((a,b)=> b.course.localeCompare(a.course) || b.total-a.total);
 export default sortedData;
